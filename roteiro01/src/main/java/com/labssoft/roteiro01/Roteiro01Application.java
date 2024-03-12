e2b6faa17b9e76e2c9c246a14f2b4781a1da2da8
José Victor Mendes Dias <jose@DPCJOSEVICTORDI>
2024-03-12 19:49:29 -0300

    adding list task endpoint

--- roteiro01/src/main/java/com/labssoft/roteiro01/Roteiro01Application.java
+++ roteiro01/src/main/java/com/labssoft/roteiro01/Roteiro01Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Roteiro01Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Roteiro01Application.class, args);
 	}
-
 }
