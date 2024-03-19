d485da293889feaf51844be090fc05e0b8408af6
José Victor Mendes Dias <jose@DPCJOSEVICTORDI>
2024-03-19 19:43:50 -0300

    adding api documentation to task controller

--- roteiro01/src/main/java/com/labssoft/roteiro01/controller/TaskController.java
+++ roteiro01/src/main/java/com/labssoft/roteiro01/controller/TaskController.java
@@ -3,9 +3,12 @@ package com.labssoft.roteiro01.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 public class TaskController {
     @GetMapping("/task")
+    @Operation(summary = "Lista todas as tarefas da lista")
     public String listAll() {
         return "Listar todas as tasks";
     }
